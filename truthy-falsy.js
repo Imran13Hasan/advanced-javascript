53626fb89dff9d969412ad8cd4d3a8e2076f4659
Imran13Hasan <imran13hasan@gmail.com>
2020-06-12 06:22:30 +0600

    true vs false

---
+++ truthy-falsy.js
@@ -0,0 +1,24 @@
+    ///Falsy...
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+    ///Truthy...
+//"0"
+//" "
+//[]
+//{}
+//"false"
+
+
+const name = 12;
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
