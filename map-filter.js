c83c051a771751446f88acf52d9933412173e059
Imran13Hasan <imran13hasan@gmail.com>
2020-06-19 12:30:07 +0600

    map-filter

--- map-filter.js
+++ map-filter.js
@@ -1,85 +1,85 @@
-// // Square numbers by simple for-loop method...
+// Square numbers by simple for-loop method...
 
-// const numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11];
-// const output = [];
+const numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11];
+const output = [];
 
-// for(let i = 0; i < numbers.length; i++){
-//     const element = numbers[i];
-//     const result = element * element;
-//     output.push(result);
-// }
-// console.log(output);
-
-
-
-
-// Square numbers using map..., map can be applied upon arrays...
-// const numbers = [3, 4, 5, 6, 7, 8];
-
-// function square (element){     //Alternative function...
-//     return element * element;
-// }
-
-// const result = numbers.map(function(element, index, array){     //Map can bear 3 parameters like element, index, array...
-//     // console.log(element);
-//     return element * element;
-// })
-
-// const square = element => element * element;    //Alternative function...
-
-// const square = numbers.map(x => x * x);      //Alternative function...
-
-// console.log(square);
+for(let i = 0; i < numbers.length; i++){
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+console.log(output);
 
 
 
 
+Square numbers using map..., map can be applied upon arrays...
+const numbers = [3, 4, 5, 6, 7, 8];
 
+function square (element){     //Alternative function...
+    return element * element;
+}
 
-// //Get different type of numbers using FILTER...
-// const numbers = [3, 4, 5, 6, 7, 8];
-// const oddNum = numbers.filter(x => x%2);
-// console.log(oddNum);
+const result = numbers.map(function(element, index, array){     //Map can bear 3 parameters like element, index, array...
+    // console.log(element);
+    return element * element;
+})
 
+const square = element => element * element;    //Alternative function...
 
-// const num = [3, 4, 5, 6, 7, 8];
-// const bigger = numbers.filter(x => x>5);
-// console.log(bigger);
+const square = numbers.map(x => x * x);      //Alternative function...
 
-
-
-
-// //For-each method...
-// const numbers = [5, 4, 2, 3, 8];
-// // numbers.forEach(element => console.log(element));
-// numbers.forEach(function(element){
-//     console.log(element);
-// })
-
-// Expected result:
-//                 5
-//                 4
-//                 2
-//                 3
-//                 8
+console.log(square);
 
 
 
 
 
-// //Reduce...
-// const arr = ['a', 'b', 'c'];
-// const reducer = (accumulator, currentValue) => accumulator + currentValue;   //The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in single output value.
-// console.log(arr.reduce(reducer));
+
+//Get different type of numbers using FILTER...
+const numbers = [3, 4, 5, 6, 7, 8];
+const oddNum = numbers.filter(x => x%2);
+console.log(oddNum);
 
 
-// const num = [1, 2, 03, 04, 05, 06]
-// const reduceNum = (accumulator, currentValue) => accumulator + currentValue;
-// console.log(num.reduce(reduceNum));
+const num = [3, 4, 5, 6, 7, 8];
+const bigger = numbers.filter(x => x>5);
+console.log(bigger);
 
-// const numMore = [1, 2, 03, 04, 05, 06]
-// const reduceNumMore = (accumulator, currentValue) => accumulator + currentValue;
-// console.log(num.reduce(reduceNumMore, 20));
+
+
+
+//For-each method...
+const numbers = [5, 4, 2, 3, 8];
+// numbers.forEach(element => console.log(element));
+numbers.forEach(function(element){
+    console.log(element);
+})
+
+Expected result:
+                5
+                4
+                2
+                3
+                8
+
+
+
+
+
+//Reduce...
+const arr = ['a', 'b', 'c'];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;   //The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in single output value.
+console.log(arr.reduce(reducer));
+
+
+const num = [1, 2, 03, 04, 05, 06]
+const reduceNum = (accumulator, currentValue) => accumulator + currentValue;
+console.log(num.reduce(reduceNum));
+
+const numMore = [1, 2, 03, 04, 05, 06]
+const reduceNumMore = (accumulator, currentValue) => accumulator + currentValue;
+console.log(num.reduce(reduceNumMore, 20));
 
 
 
